68f3460e7c79a7f6f508721d70ab8d2ed963363e
Nick Radonic <big.rad@gmail.com>
2014-03-16 01:57:33 -0400

    revise comparison and extend to work around periphery of display
    revise comparison and extend to work around periphery of display

--- fillCanvas.js
+++ fillCanvas.js
@@ -3,7 +3,7 @@ function rand1(){return Math.floor(Math.random()*256);}
 function drawCanvas1(){
 	var c=document.getElementById("drawHere");
 	var ctx=c.getContext("2d");
-	gridSize = 200;
+	gridSize = 100;
 	gridSize2 = gridSize*gridSize*4;
 	
 	var canvasData = ctx.getImageData(0,0,gridSize, gridSize);
@@ -21,83 +21,93 @@ function smooth(){
 	var ctx=c.getContext("2d");
 	canvasData = ctx.getImageData(0,0,gridSize, gridSize);
 
-	function swapTestHor(indexST){
- 		var STLr = (canvasData.data[indexST]-canvasData.data[indexST+4]);
- 		var STLb = (canvasData.data[indexST+1]-canvasData.data[indexST+5]);
- 		var STLg = (canvasData.data[indexST+2]-canvasData.data[indexST+6]);
- 		var STRr = (canvasData.data[indexST+8]-canvasData.data[indexST+12]);
- 		var STRg = (canvasData.data[indexST+9]-canvasData.data[indexST+13]);
- 		var STRb = (canvasData.data[indexST+10]-canvasData.data[indexST+14]);
-        var STLeft = STLr*STLr+STLg*STLg+STLb*STLb;
+	function calculateIndexST(i, j){ return (j+i*gridSize)*4;}
+	
+	function swapTest(STi, STj, incStep){
+		var indexST = calculateIndexST(STi, STj);
+ 		var STLr = (canvasData.data[indexST]-canvasData.data[indexST+incStep]);
+ 		var STLg = (canvasData.data[indexST+1]-canvasData.data[indexST+incStep+1]);
+ 		var STLb = (canvasData.data[indexST+2]-canvasData.data[indexST+incStep+2]);
+ 		var STRr = (canvasData.data[indexST+2*incStep]-canvasData.data[indexST+incStep*3]);
+ 		var STRg = (canvasData.data[indexST+2*incStep+1]-canvasData.data[indexST+incStep*3+1]);
+ 		var STRb = (canvasData.data[indexST+2*incStep+2]-canvasData.data[indexST+incStep*3+2]);
+        var STLeft = STLr*STLr+STLg*STLg+STLb*STLb + STRr*STRr +STRg*STRg+STRb*STRb;
  
-  		STLr = (canvasData.data[indexST]-canvasData.data[indexST+8]);
- 		STLb = (canvasData.data[indexST+1]-canvasData.data[indexST+9]);
- 		STLg = (canvasData.data[indexST+2]-canvasData.data[indexST+10]);
- 		STRr = (canvasData.data[indexST+12]-canvasData.data[indexST+4]);
- 		STRg = (canvasData.data[indexST+13]-canvasData.data[indexST+5]);
- 		STRb = (canvasData.data[indexST+14]-canvasData.data[indexST+6]);
-        var STRight = STLr*STLr+STLg*STLg+STLb*STLb;
+  		STLr = (canvasData.data[indexST]-canvasData.data[indexST+2*incStep]);
+ 		STLg = (canvasData.data[indexST+1]-canvasData.data[indexST+2*incStep+1]);
+ 		STLb = (canvasData.data[indexST+2]-canvasData.data[indexST+2*incStep+2]);
+ 		STRr = (canvasData.data[indexST+3*incStep]-canvasData.data[indexST+incStep]);
+ 		STRg = (canvasData.data[indexST+3*incStep+1]-canvasData.data[indexST+incStep+1]);
+ 		STRb = (canvasData.data[indexST+3*incStep+2]-canvasData.data[indexST+incStep+2]);
+        var STRight = STLr*STLr+STLg*STLg+STLb*STLb + STRr*STRr +STRg*STRg+STRb*STRb;
+		return STRight>STLeft;
+	};
+	
+	function swapTestEdge(STi, STj, incStep){
+		var indexST = calculateIndexST(STi, STj);
+ 		var STRr = (canvasData.data[indexST+2*incStep]-canvasData.data[indexST+incStep]);
+ 		var STRg = (canvasData.data[indexST+2*incStep+1]-canvasData.data[indexST+incStep+1]);
+ 		var STRb = (canvasData.data[indexST+2*incStep+2]-canvasData.data[indexST+incStep+2]);
+        var STLeft = STRr*STRr +STRg*STRg+STRb*STRb;
+ 
+  		STRr = (canvasData.data[indexST+2*incStep]-canvasData.data[indexST]);
+ 		STRg = (canvasData.data[indexST+2*incStep+1]-canvasData.data[indexST+1]);
+ 		STRb = (canvasData.data[indexST+2*incStep+2]-canvasData.data[indexST+2]);
+        var STRight = STRr*STRr +STRg*STRg+STRb*STRb;
 		return STRight>STLeft;
 	};
 
-	function swapTestVert(indexST){
- 		var STLr = (canvasData.data[indexST]-canvasData.data[indexST+4*gridSize]);
- 		var STLb = (canvasData.data[indexST+1]-canvasData.data[indexST+4*gridSize+1]);
- 		var STLg = (canvasData.data[indexST+2]-canvasData.data[indexST+4*gridSize+2]);
- 		var STRr = (canvasData.data[indexST+8*gridSize]-canvasData.data[indexST+12*gridSize]);
- 		var STRg = (canvasData.data[indexST+8*gridSize+1]-canvasData.data[indexST+12*gridSize+1]);
- 		var STRb = (canvasData.data[indexST+8*gridSize+2]-canvasData.data[indexST+12*gridSize+2]);
-        var STLeft = STLr*STLr+STLg*STLg+STLb*STLb;
- 
-  		STLr = (canvasData.data[indexST]-canvasData.data[indexST+8*gridSize]);
- 		STLb = (canvasData.data[indexST+1]-canvasData.data[indexST+8*gridSize+1]);
- 		STLg = (canvasData.data[indexST+2]-canvasData.data[indexST+8*gridSize+2]);
- 		STRr = (canvasData.data[indexST+12*gridSize]-canvasData.data[indexST+4*gridSize]);
- 		STRg = (canvasData.data[indexST+12*gridSize+1]-canvasData.data[indexST+4*gridSize+1]);
- 		STRb = (canvasData.data[indexST+12*gridSize+2]-canvasData.data[indexST+4*gridSize+2]);
-        var STRight = STLr*STLr+STLg*STLg+STLb*STLb;
-		return STRight>STLeft;
+	function swapCellsInterior(STi, STj, incStep){
+		var indexST = calculateIndexST(STi, STj);
+		var r = canvasData.data[indexST+incStep];
+		var g = canvasData.data[indexST+incStep+1];
+		var b = canvasData.data[indexST+incStep+2];
+		var a = canvasData.data[indexST+incStep+3];
+		canvasData.data[indexST+incStep]=canvasData.data[indexST+2*incStep];
+		canvasData.data[indexST+incStep+1]=canvasData.data[indexST+2*incStep+1];
+		canvasData.data[indexST+incStep+2]=canvasData.data[indexST+2*incStep+2];
+		canvasData.data[indexST+incStep+3]=canvasData.data[indexST+2*incStep+3];
+		canvasData.data[indexST+2*incStep]=r;
+		canvasData.data[indexST+2*incStep+1]=g;
+		canvasData.data[indexST+2*incStep+2]=b;
+		canvasData.data[indexST+2*incStep+3]=a;
 	};
 
-	function swapHorizontal(indexH){
-		var r = canvasData.data[index0+4];
-		var g = canvasData.data[index0+5];
-		var b = canvasData.data[index0+6];
-		var a = canvasData.data[index0+7];
-		canvasData.data[index0+4]=canvasData.data[index0+8];
-		canvasData.data[index0+5]=canvasData.data[index0+9];
-		canvasData.data[index0+6]=canvasData.data[index0+10];
-		canvasData.data[index0+7]=canvasData.data[index0+11];
-		canvasData.data[index0+8]=r;
-		canvasData.data[index0+9]=g;
-		canvasData.data[index0+10]=b;
-		canvasData.data[index0+11]=a;
+	function swapEdgeBlock(STi, STj, incStep){
+		var indexST = calculateIndexST(STi, STj);
+		var r = canvasData.data[indexST+incStep];
+		var g = canvasData.data[indexST+incStep+1];
+		var b = canvasData.data[indexST+incStep+2];
+		var a = canvasData.data[indexST+incStep+3];
+		canvasData.data[indexST+incStep]=canvasData.data[indexST];
+		canvasData.data[indexST+incStep+1]=canvasData.data[indexST+1];
+		canvasData.data[indexST+incStep+2]=canvasData.data[indexST+2];
+		canvasData.data[indexST+incStep+3]=canvasData.data[indexST+3];
+		canvasData.data[indexST]=r;
+		canvasData.data[indexST+1]=g;
+		canvasData.data[indexST+2]=b;
+		canvasData.data[indexST+3]=a;
 	};
 
-	function swapVertical(indexH){
-		var r = canvasData.data[index0+4*gridSize];
-		var g = canvasData.data[index0+4*gridSize+1];
-		var b = canvasData.data[index0+4*gridSize+2];
-		var a = canvasData.data[index0+4*gridSize+3];
-		canvasData.data[index0+4*gridSize]=canvasData.data[index0+8*gridSize];
-		canvasData.data[index0+4*gridSize+1]=canvasData.data[index0+8*gridSize+1];
-		canvasData.data[index0+4*gridSize+2]=canvasData.data[index0+8*gridSize+2];
-		canvasData.data[index0+4*gridSize+3]=canvasData.data[index0+8*gridSize+3];
-		canvasData.data[index0+8*gridSize]=r;
-		canvasData.data[index0+8*gridSize+1]=g;
-		canvasData.data[index0+8*gridSize+2]=b;
-		canvasData.data[index0+8*gridSize+3]=a;
-	};
-
-//	for(var k=0; k<200; k++){
-		for(var i=0; i<gridSize-3;i++){
-			for(var j=0; j< gridSize-3;j++){
-				var index0 = (j+i*gridSize)*4;
-				if(swapTestHor(index0)){swapHorizontal(index0);}
-				if(swapTestVert(index0)){swapVertical(index0);}
-			}
+	// internal box i=row, j=columns
+	for(var i=0; i<gridSize-4;i++){
+		for(var j=0; j< gridSize-4;j++){
+			if(swapTest(i,j,4)){swapCellsInterior(i,j,4);}
+			if(swapTest(i,j,gridSize*4)){swapCellsInterior(i,j,gridSize*4);}
 		}
-		ctx.putImageData(canvasData,0,0);
+	}
+	// left side
+	for ( var i = 0; i< gridSize-1; i++){
+		//left edge increment=4
+		if(swapTestEdge(i,0,4)){swapEdgeBlock(i, 0, 4);}
+		//right edge increment=-4
+		if(swapTestEdge(i,gridSize-1,-4)){swapEdgeBlock(i, gridSize-1, -4);}
+		//top edge increment=gridSize
+		if(swapTestEdge(0,i,gridSize*4)){swapEdgeBlock(0, i, gridSize*4);}
+		//bottom edge increment=-gridSize
+		if(swapTestEdge(gridSize-1, i,-gridSize)){swapEdgeBlock(gridSize-1, i, -gridSize);}
+	}		
+	ctx.putImageData(canvasData,0,0);
 //	};
 
 }
