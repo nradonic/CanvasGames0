74a2f456d4d39357014cfa24afc3fe718609e49b
Nick Radonic <big.rad@gmail.com>
2014-03-22 18:27:49 -0400

    temporary version before I refactor the drawing code - doesn't do proper draw.
    temporary version before I refactor the drawing code - doesn't do
    proper draw.

--- fillCanvas.js
+++ fillCanvas.js
@@ -1,12 +1,27 @@
-function rand1(){return Math.floor(Math.random()*2)*255;}
+function rand1(){
+	return Math.floor(Math.random()*4)*85;
+}
+
 var flipThreshold = 0.5;
-var gridSize = 10;
+var gridSize = 3;
 var gridSize2 = gridSize*gridSize;
-function gridCell(){this.r=rand1(),this.g=0,this.b=0};
+function gridCell(rc,gc,bc){this.r=rc,this.g=gc,this.b=bc};
 
 var dataGrid = new Array(gridSize2);
-for(i=0;i<gridSize2;i++){dataGrid[i]=new gridCell();} 
 
+
+
+function OnChange(dropdown)
+{
+    var myindex  = dropdown.selectedIndex;
+    var SelValue = dropdown.options[myindex].value;
+    gridSize = parseInt(SelValue);
+    gridSize2 = gridSize*gridSize;   
+    dataGrid = new Array(gridSize2);
+    for(i=0;i<gridSize2;i++){dataGrid[i]=new gridCell(rand1(),0,0);} 
+    flipThreshold =0.5; 
+    return true;
+}
 function drawCanvas1(){
 	var c=document.getElementById("drawHere");
 	var ctx=c.getContext("2d");
@@ -17,8 +32,8 @@ function drawCanvas1(){
 	var canvasData = ctx.getImageData(0,0,myScreen, myScreen);
 	
 	for(var i = 0; i< myScreen2;i+=4){
-		var iS = Math.floor(Math.floor(i/myScreen4)*gridSize/myScreen);
-		var jS = Math.floor((i%myScreen4)*gridSize/myScreen4)+iS;
+		var iS = Math.floor(Math.floor(i/myScreen4 + 0.00001)*gridSize/myScreen+0.00001);
+		var jS = Math.floor(((i+0.00001)%myScreen4)*gridSize/myScreen4+0.00001)+iS;
 	
 		canvasData.data[i]=dataGrid[jS].r;
 		canvasData.data[i+1]=dataGrid[jS].g;
@@ -28,86 +43,96 @@ function drawCanvas1(){
 	ctx.putImageData(canvasData,0,0);
 };
 
+function calculateIndexST(i, j){ return (j+i*gridSize);}
 
-
-function smooth(){
+function swapTestEdge(STi, STj, incStep){
+	var indexST = calculateIndexST(STi, STj);
+	var zero = indexST;
+	var one = indexST+incStep;
+	var two = indexST+2*incStep;
 	
-	flipThreshold = flipThreshold*0.95;
-	document.getElementById("Threshold").innerHTML=flipThreshold;
-	
-	function calculateIndexST(i, j){ return (j+i*gridSize);}
-	
-	function swapTest(STi, STj, incStep){
-		var indexST = calculateIndexST(STi, STj);
- 		var STLr = dataGrid[indexST].r-dataGrid[indexST+incStep].r;
- 		var STLg = dataGrid[indexST].g-dataGrid[indexST+incStep].g;
- 		var STLb = dataGrid[indexST].b-dataGrid[indexST+incStep].b;
- 		
- 		var STRr = dataGrid[indexST+2*incStep].r-dataGrid[indexST+incStep*3].r;
- 		var STRg = dataGrid[indexST+2*incStep].g-dataGrid[indexST+incStep*3].g;
- 		var STRb = dataGrid[indexST+2*incStep].b-dataGrid[indexST+incStep*3].b;
- 		
-        var STLeft = STLr*STLr+STLg*STLg+STLb*STLb + STRr*STRr +STRg*STRg+STRb*STRb;
- 
-		STLr = dataGrid[indexST+3*incStep].r-dataGrid[indexST+incStep].r;
- 		STLg = dataGrid[indexST+3*incStep].g-dataGrid[indexST+incStep].g;
- 		STLb = dataGrid[indexST+3*incStep].b-dataGrid[indexST+incStep].b;
- 		
- 		STRr = dataGrid[indexST+2*incStep].r-dataGrid[indexST].r;
- 		STRg = dataGrid[indexST+2*incStep].g-dataGrid[indexST].g;
- 		STRb = dataGrid[indexST+2*incStep].b-dataGrid[indexST].b;
+	var STLr = dataGrid[zero].r-dataGrid[one].r;
+	var STLg = dataGrid[zero].g-dataGrid[one].g;
+	var STLb = dataGrid[zero].b-dataGrid[one].b;
+    var STLeft = STLr*STLr +STLg*STLg+STLb*STLb;
 
-  		var STRight = STLr*STLr+STLg*STLg+STLb*STLb + STRr*STRr +STRg*STRg+STRb*STRb;
-  		
-  		var Bbool =  STRight>STLeft;
-	    Bbool = (Math.random()<flipThreshold)||Bbool;
-	    return Bbool;
-	};
+	var STRr = dataGrid[two].r-dataGrid[zero].r;
+	var STRg = dataGrid[two].g-dataGrid[zero].g;
+	var STRb = dataGrid[two].b-dataGrid[zero].b;
+    var STRight = STRr*STRr +STRg*STRg +STRb*STRb;
+    var Bbool =  STRight>STLeft; 
+    //Bbool = (Math.random()>flipThreshold)||Bbool;
+    return Bbool;
+};
 	
-	function swapTestEdge(STi, STj, incStep){
-		var indexST = calculateIndexST(STi, STj);
- 		var STLr = dataGrid[indexST].r-dataGrid[indexST+incStep].r;
- 		var STLg = dataGrid[indexST].g-dataGrid[indexST+incStep].g;
- 		var STLb = dataGrid[indexST].b-dataGrid[indexST+incStep].b;
-
-        var STLeft = STRr*STRr +STRg*STRg+STRb*STRb;
- 
-  		var STRr = dataGrid[indexST+2*incStep].r-dataGrid[indexST].r;
- 		var STRg = dataGrid[indexST+2*incStep].g-dataGrid[indexST].g;
- 		var STRb = dataGrid[indexST+2*incStep].b-dataGrid[indexST].b;
-        var STRight = STRr*STRr +STRg*STRg+STRb*STRb;
-        var Bbool =  STRight>STLeft;
-        Bbool = (Math.random()>flipThreshold)||Bbool;
-	    return Bbool;
-	};
-
-	function swapCells(STi, STj, incStep){
-		var indexST = calculateIndexST(STi, STj);
-		var r = dataGrid[indexST+incStep].r;
-		var g = dataGrid[indexST+incStep].g;
-		var b = dataGrid[indexST+incStep].b;
-		dataGrid[indexST+incStep].r=dataGrid[indexST+2*incStep].r;
-		dataGrid[indexST+incStep].g=dataGrid[indexST+2*incStep].g;
-		dataGrid[indexST+incStep].b=dataGrid[indexST+2*incStep].b;
+function swapTest(STi, STj, incStep){
+	var indexST = calculateIndexST(STi, STj);
+	var zero = indexST;
+	var one = indexST+incStep;
+	var two = indexST+2*incStep;
+	var three = indexST+3*incStep;
+	
+	var STLr = dataGrid[zero].r-dataGrid[one].r;
+	var STLg = dataGrid[zero].g-dataGrid[one].g;
+	var STLb = dataGrid[zero].b-dataGrid[one].b;
+	
+	var STRr = dataGrid[two].r-dataGrid[three].r;
+	var STRg = dataGrid[two].g-dataGrid[three].g;
+	var STRb = dataGrid[two].b-dataGrid[three].b;
 		
-		dataGrid[indexST+2*incStep].r=r;
-		dataGrid[indexST+2*incStep].g=g;
-		dataGrid[indexST+2*incStep].b=b;
-	};
+    var STLeft = STLr*STLr +STLg*STLg +STLb*STLb +STRr*STRr +STRg*STRg +STRb*STRb;
 
+	STLr = dataGrid[three].r-dataGrid[one].r;
+	STLg = dataGrid[three].g-dataGrid[one].g;
+	STLb = dataGrid[three].b-dataGrid[one].b;
 	
-	// internal box i=row, j=columns
+	STRr = dataGrid[two].r-dataGrid[zero].r;
+	STRg = dataGrid[two].g-dataGrid[zero].g;
+	STRb = dataGrid[two].b-dataGrid[zero].b;
+
+	var STRight = STLr*STLr +STLg*STLg +STLb*STLb +STRr*STRr +STRg*STRg +STRb*STRb;
+	var Bbool =  STRight>STLeft;
+    //Bbool = (Math.random()<flipThreshold)||Bbool;
+    return Bbool;
+};		
+
+function swapCells(STi, STj, incStep){
+	var indexST = calculateIndexST(STi, STj);
+	var zero = indexST;
+	var one = indexST+incStep;
+	var two = indexST+2*incStep;
+	
+	var r = dataGrid[one].r;
+	var g = dataGrid[one].g;
+	var b = dataGrid[one].b;
+	
+	dataGrid[one].r=dataGrid[two].r;
+	dataGrid[one].g=dataGrid[two].g;
+	dataGrid[one].b=dataGrid[two].b;
+	
+	dataGrid[two].r=r;
+	dataGrid[two].g=g;
+	dataGrid[two].b=b;
+	drawCanvas1();
+};
+
+function interiorHorizontal(){	// internal box i=row, j=columns
 	for(var i=0; i<gridSize-1;i++){
-		for(var j=0; j< gridSize-4;j++){
+		for(var j=0; j< gridSize-3;j++){
 			if(swapTest(i,j,1)){swapCells(i,j,1);}
 		}
 	}
-	for(var i=0; i<gridSize-4;i++){
+}
+
+function interiorVertical(){
+	for(var i=0; i<gridSize-3;i++){
 		for(var j=0; j< gridSize-1;j++){
 			if(swapTest(i,j,gridSize)){swapCells(i,j,gridSize);}
 		}
 	}
-	// left side
+}
+
+function edgewalk(){	// left side
 	for ( var i = 0; i< gridSize-1; i++){
 		//left edge increment=1
 		if(swapTestEdge(i,2,-1)){swapCells(i, 2, -1);}
@@ -118,9 +143,23 @@ function smooth(){
 		//bottom edge increment=-gridSize
 		if(swapTestEdge(gridSize-3, i,gridSize)){swapCells(gridSize-3, i, gridSize);}
 	}		
-
 }
 
-function cycle(myVarr){if(flipThreshold>0.0001){ drawCanvas1(); smooth();}else{clearInterval(myVarr);}}
+function smooth(){
+	flipThreshold = flipThreshold*0.95;
+	document.getElementById("Threshold").innerHTML=flipThreshold;
+	interiorHorizontal();
+	interiorVertical();
+	edgewalk();
+}
+
+function cycle(myVarr){
+	if(flipThreshold>0.0001){ smooth();}else{clearInterval(myVarr);} ;
+	drawCanvas1(); 
+}
+
+OnChange(document.getElementById("select1"));
+drawCanvas1();
 var myVar=setInterval(function(){cycle(myVar)},1000);
+
 //smooth();
