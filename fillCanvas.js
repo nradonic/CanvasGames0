0000ecfe4c77053d3321960bf02ed1ba03ca92c5
Nick Radonic <big.rad@gmail.com>
2014-04-03 15:59:31 -0400

    adult neighbor weighting - side by side more important than diagonal match

--- fillCanvas.js
+++ fillCanvas.js
@@ -224,13 +224,13 @@ function fitColor(i1,j1,i2,j2){
 	var total = 0;
 	var cnt = 0;
 	if(j2!== 0){total = diffSQ(ST1, ST2-1); cnt++;}
-	if(i2!==0 && j2!==0){total += diffSQ(ST1,ST2-gridSize-1); cnt++;}//2;
+	if(i2!==0 && j2!==0){total += diffSQ(ST1,ST2-gridSize-1)/2; cnt++;}//2;
 	if(i2!==0){total += diffSQ(ST1,ST2-gridSize); cnt++;}
-	if(i2!==0 && j2!==edge){total += diffSQ(ST1,ST2-gridSize+1); cnt++;}///2;
+	if(i2!==0 && j2!==edge){total += diffSQ(ST1,ST2-gridSize+1)/2; cnt++;}///2;
 	if(j2!==edge){total += diffSQ(ST1,ST2+1); cnt++;}
-	if(i2!==edge && j2!==0){total += diffSQ(ST1,ST2+gridSize-1); cnt++;}//2;
+	if(i2!==edge && j2!==0){total += diffSQ(ST1,ST2+gridSize-1)/2; cnt++;}//2;
 	if(i2!==edge){total += diffSQ(ST1,ST2+gridSize); cnt++;}
-	if(i2!==edge && j2!==edge){total += diffSQ(ST1,ST2+gridSize+1); cnt++;}//2;
+	if(i2!==edge && j2!==edge){total += diffSQ(ST1,ST2+gridSize+1)/2; cnt++;}//2;
 	return total/cnt;
 }
 
